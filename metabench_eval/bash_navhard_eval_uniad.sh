7c1303d9623d81d6f2ac490e0f44f6a8340cf57e
Hongwei Ruan <hwruan@umich.edu>
2026-02-17 14:25:17 -0800

    remove rr 35 40 from uniad and vad

--- metabench_eval/bash_navhard_eval_uniad.sh
+++ metabench_eval/bash_navhard_eval_uniad.sh
@@ -6,7 +6,7 @@ NUM_SAMPLES=5
 export ALPHA ALPHA_SAFE NUM_SAMPLES
 
 for SPLIT in 210-1 211-1; do
-  for REPLAN_RATE in 1 2 5 10 15 20 25 30 35 40; do
+  for REPLAN_RATE in 1 2 5 10 15 20 25 30; do
     for EVAL_FRAMES in 40 80; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
       envsubst < ./job_navhard_eval_uniad.yaml | kubectl apply -f -
