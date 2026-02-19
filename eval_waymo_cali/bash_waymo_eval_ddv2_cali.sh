baefbdd6adeb529718946004e08230773a005e47
Hongwei Ruan <hwruan@umich.edu>
2026-02-19 01:50:21 -0800

    Add: eval_advbmt_navhard_cali: transfuser ; eval_waymo and eval_waymo_cali

---
+++ eval_waymo_cali/bash_waymo_eval_ddv2_cali.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ALPHA=0.5
+ALPHA_SAFE=0-5
+NUM_SAMPLES=5
+export ALPHA ALPHA_SAFE NUM_SAMPLES
+
+for SPLIT in 200-1 200-2; do
+  for REPLAN_RATE in 1 2 5 10 15 20 25 30 35 40; do
+    for EVAL_FRAMES in 40 80; do
+      export SPLIT REPLAN_RATE EVAL_FRAMES
+      envsubst < ./job_waymo_eval_ddv2_cali.yaml | kubectl apply -f -
+    done
+  done
+done
