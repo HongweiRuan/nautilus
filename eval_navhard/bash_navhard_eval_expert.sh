72a93deb9039df8370d5429cf6cce6421ec3ec19
Hongwei Ruan <hwruan@umich.edu>
2026-03-05 12:51:18 -0800

    Refactor evaluation scripts and job configurations for Waymo and Navhard, updating parameters and enabling GPU nodes

--- eval_navhard/bash_navhard_eval_expert.sh
+++ eval_navhard/bash_navhard_eval_expert.sh
@@ -8,15 +8,21 @@ export ALPHA ALPHA_SAFE NUM_SAMPLES
 
 # Expert evaluation parameter combinations - fixed pairs
 for SPLIT in 210-1 211-1; do
-  # Pair 1: EVAL_FRAMES=40 with EGO_REPLAY_FRAMES=60
-  EGO_REPLAY_FRAMES=60
-  EVAL_FRAMES=40
+  # Pair 1: EVAL_FRAMES=120 with EGO_REPLAY_FRAMES=140
+  EGO_REPLAY_FRAMES=140
+  EVAL_FRAMES=120
+  export SPLIT EGO_REPLAY_FRAMES EVAL_FRAMES
+  envsubst < ./job_navhard_eval_expert.yaml | kubectl apply -f -
+
+  # Pair 3: EVAL_FRAMES=160 with EGO_REPLAY_FRAMES=180
+  EGO_REPLAY_FRAMES=180
+  EVAL_FRAMES=160
   export SPLIT EGO_REPLAY_FRAMES EVAL_FRAMES
   envsubst < ./job_navhard_eval_expert.yaml | kubectl apply -f -
   
-  # Pair 2: EVAL_FRAMES=80 with EGO_REPLAY_FRAMES=100
-  EGO_REPLAY_FRAMES=100
-  EVAL_FRAMES=80
+  # Pair 3: EVAL_FRAMES=200 with EGO_REPLAY_FRAMES=220
+  EGO_REPLAY_FRAMES=220
+  EVAL_FRAMES=200
   export SPLIT EGO_REPLAY_FRAMES EVAL_FRAMES
   envsubst < ./job_navhard_eval_expert.yaml | kubectl apply -f -
 done
--- eval_waymo/bash_waymo_eval_rap.sh
+++ eval_waymo/bash_waymo_eval_rap.sh
@@ -5,11 +5,11 @@ ALPHA_SAFE=0-5
 NUM_SAMPLES=5
 export ALPHA ALPHA_SAFE NUM_SAMPLES
 
-for SPLIT in 210-1 211-1; do
+for SPLIT in 200-1 200-2; do
   for REPLAN_RATE in 5; do
-    for EVAL_FRAMES in 120 160 200; do
+    for EVAL_FRAMES in 80; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
-      envsubst < ./job_navhard_eval_transfuser.yaml | kubectl apply -f -
+      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES}' < ./job_waymo_eval_rap.yaml | kubectl apply -f -
     done
   done
 done
