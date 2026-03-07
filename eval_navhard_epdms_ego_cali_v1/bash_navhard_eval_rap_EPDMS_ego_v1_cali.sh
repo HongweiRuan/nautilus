4ceb281975dc36aedfc57b8bbda788d3515bb643
Hongwei Ruan <hwruan@umich.edu>
2026-03-06 18:57:54 -0800

    Add evaluation scripts and Kubernetes job configurations for Waymo and Navhard
    - Created bash scripts for evaluating EPDMS and RAP models in Navhard and Waymo datasets.
    - Added Kubernetes job YAML configurations for running evaluations with specified parameters.
    - Implemented resource requests and limits for GPU and memory in job specifications.
    - Included necessary package installations and environment setups in job containers.
    - Configured node affinity and tolerations for optimal scheduling on specific GPU nodes.

--- eval_navhard_epdms_ego_cali_v1/bash_navhard_eval_rap_EPDMS_ego_v1_cali.sh
+++ eval_navhard_epdms_ego_cali_v1/bash_navhard_eval_rap_EPDMS_ego_v1_cali.sh
@@ -8,9 +8,9 @@ export ALPHA ALPHA_SAFE NUM_SAMPLES NUM_GROUPS
 
 for SPLIT in 210-1 211-1; do
   for REPLAN_RATE in 5; do
-    for EVAL_FRAMES in 120 160; do
+    for EVAL_FRAMES in 40 80 120 160; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
-      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES} ${NUM_GROUPS}' < ./job_navhard_eval_dd_EPDMS_ego_v1_cali.yaml | kubectl apply -f -
+      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES} ${NUM_GROUPS}' < ./job_navhard_eval_rap_EPDMS_ego_v1_cali.yaml | kubectl apply -f -
     done
   done
 done
