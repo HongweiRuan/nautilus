7ca3e8c1e66bab7d131e34479b42b73cfe5212d3
Hongwei Ruan <hwruan@umich.edu>
2026-03-11 18:19:42 -0700

    Refine evaluation scripts for navhard scenarios by limiting REPLAN_RATE and EVAL_FRAMES, and update GPU hostnames in job configurations

--- eval_navhard/bash_navhard_eval_uniad.sh
+++ eval_navhard/bash_navhard_eval_uniad.sh
@@ -6,8 +6,8 @@ NUM_SAMPLES=5
 export ALPHA ALPHA_SAFE NUM_SAMPLES
 
 for SPLIT in 210-1 211-1; do
-  for REPLAN_RATE in 1 2 5 10 15 20 25 30; do
-    for EVAL_FRAMES in 40 80; do
+  for REPLAN_RATE in 5; do
+    for EVAL_FRAMES in 80; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
       envsubst < ./job_navhard_eval_uniad.yaml | kubectl apply -f -
     done
