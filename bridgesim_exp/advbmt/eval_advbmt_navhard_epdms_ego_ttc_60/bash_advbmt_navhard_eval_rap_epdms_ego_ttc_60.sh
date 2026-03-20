f907b8bac82dbedab99e3be0a9825b7d5de3b4d1
Hongwei Ruan <hwruan@umich.edu>
2026-03-20 03:17:04 -0700

    Add Kubernetes job configurations for NuScenes and Waymo evaluations
    - Created job_nuscenes_eval_ddv2_eac.yaml for evaluating diffusion drive v2 on NuScenes with adaptive scoring.
    - Created job_nuscenes_eval_rap_eac.yaml for evaluating RAP model on NuScenes with adaptive scoring.
    - Added bash scripts for executing Waymo evaluations for DD and DDv2 models with adaptive scoring.
    - Implemented job configurations for evaluating DD and DDv2 models on Waymo with adaptive scoring.
    - Added job configuration for evaluating RAP model on Waymo with adaptive scoring.

--- bridgesim_exp/advbmt/eval_advbmt_navhard_epdms_ego_ttc_60/bash_advbmt_navhard_eval_rap_epdms_ego_ttc_60.sh
+++ bridgesim_exp/advbmt/eval_advbmt_navhard_epdms_ego_ttc_60/bash_advbmt_navhard_eval_rap_epdms_ego_ttc_60.sh
@@ -7,10 +7,10 @@ NUM_GROUPS=1
 export ALPHA ALPHA_SAFE NUM_SAMPLES NUM_GROUPS
 
 for SPLIT in 210-1 211-1; do
-  for REPLAN_RATE in 1 5; do
+  for REPLAN_RATE in 5; do
     for EVAL_FRAMES in 80; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
-      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES}' < ./job_advbmt_navhard_eval_rap_epdms_ego_adaptive_col_60.yaml | kubectl apply -f -
+      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES}' < ./job_advbmt_navhard_eval_rap_epdms_ego_ttc_60.yaml | kubectl apply -f -
     done
   done
 done
