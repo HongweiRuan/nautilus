062f7e003779a577b83920354a075f8e6f6ef3f4
Hongwei Ruan <hwruan@umich.edu>
2026-03-22 02:11:27 -0700

    Add evaluation jobs for navhard scenarios with new configurations
    - Created job_navhard_eval_ddv2_epdms_ego_ttc_2.yaml for evaluating DiffusionDriveV2 with EPDMS ego TTC.
    - Created job_navhard_eval_rap_epdms_ego_ttc_2.yaml for evaluating RAP with EPDMS ego TTC.
    - Added bash scripts for running evaluations for dd, ddv2, and rap scenarios in eval_navhard_epdms_ego_ttc_3.
    - Created job_navhard_eval_dd_epdms_ego_ttc_3.yaml for evaluating DiffusionDrive with EPDMS ego TTC.
    - Created job_navhard_eval_ddv2_epdms_ego_ttc_3.yaml for evaluating DiffusionDriveV2 with EPDMS ego TTC.
    - Created job_navhard_eval_rap_epdms_ego_ttc_3.yaml for evaluating RAP with EPDMS ego TTC.

--- bridgesim_exp/navhard_idm/eval_navhard_idm_epdms_ego_ttc_3/bash_navhard_idm_eval_dd_epdms_ego_ttc_3.sh
+++ bridgesim_exp/navhard_idm/eval_navhard_idm_epdms_ego_ttc_3/bash_navhard_idm_eval_dd_epdms_ego_ttc_3.sh
@@ -7,10 +7,10 @@ NUM_GROUPS=${NUM_GROUPS:-1}
 export ALPHA ALPHA_SAFE NUM_SAMPLES NUM_GROUPS
 
 for SPLIT in 210-1 211-1; do
-  for REPLAN_RATE in 1; do
+  for REPLAN_RATE in 5; do
     for EVAL_FRAMES in 80; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
-      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES} ${NUM_GROUPS}' < ./job_advbmt_navhard_eval_dd_epdms_ego_adaptive_col_60.yaml | kubectl apply -f -
+      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES} ${NUM_GROUPS}' < ./job_navhard_idm_eval_dd_epdms_ego_ttc_3.yaml | kubectl apply -f -
     done
   done
 done
--- bridgesim_exp/navhard_scorers/eval_navhard_epdms_ego_ttc_3/bash_navhard_eval_ddv2_epdms_ego_ttc_3.sh
+++ bridgesim_exp/navhard_scorers/eval_navhard_epdms_ego_ttc_3/bash_navhard_eval_ddv2_epdms_ego_ttc_3.sh
@@ -6,11 +6,11 @@ NUM_SAMPLES=5
 NUM_GROUPS=1
 export ALPHA ALPHA_SAFE NUM_SAMPLES NUM_GROUPS
 
-for SPLIT in 211-1; do
-  for REPLAN_RATE in 5 ; do
+for SPLIT in 210-1 211-1; do
+  for REPLAN_RATE in 5; do
     for EVAL_FRAMES in 80; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
-      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES}' < ./job_advbmt_navhard_eval_rap_epdms_ego_adaptive_col_60.yaml | kubectl apply -f -
+      envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES} ${NUM_GROUPS}' < ./job_navhard_eval_ddv2_epdms_ego_ttc_3.yaml | kubectl apply -f -
     done
   done
 done
