a6dcdf416aac92b629dcb746112755ba22bed076
Hongwei Ruan <hwruan@umich.edu>
2026-03-25 00:12:02 -0700

    Add evaluation job scripts and configurations for navhard scenarios

---
+++ bridgesim_exp/navhard_np/eval_navhard_np_cali_20/bash_navhard_eval_ddv2_np_cali_20_rr40.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+REPLAN_RATE=40
+EVAL_FRAMES=40
+export REPLAN_RATE EVAL_FRAMES
+
+for SPLIT in 210-1 211-1; do
+  for NUM_PROPOSALS in 1 2 5 10 20; do
+    export SPLIT NUM_PROPOSALS
+    envsubst '${SPLIT} ${REPLAN_RATE} ${EVAL_FRAMES} ${NUM_PROPOSALS}' < ./job_navhard_eval_ddv2_np_cali_20.yaml | kubectl apply -f -
+  done
+done
