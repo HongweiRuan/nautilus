d9cf6bd94e78c0d6c801eeabb9ca781eac17d414
Hongwei Ruan <hwruan@umich.edu>
2026-03-26 16:32:14 -0700

    Add evaluation scripts and job configurations for navhard scenarios with new output paths

--- bridgesim_exp/basic_navhard_no_scorers/eval_navhard/bash_navhard_eval_dd_simscale.sh
+++ bridgesim_exp/basic_navhard_no_scorers/eval_navhard/bash_navhard_eval_dd_simscale.sh
@@ -9,13 +9,13 @@ for SPLIT in 210-1 211-1; do
   for REPLAN_RATE in 5; do
     for EVAL_FRAMES in 80; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
-      envsubst < ./job_navhard_eval_dd.yaml | kubectl apply -f -
+      envsubst < ./job_navhard_eval_dd_simscale.yaml | kubectl apply -f -
     done
   done
   for REPLAN_RATE in 40; do
     for EVAL_FRAMES in 40; do
       export SPLIT REPLAN_RATE EVAL_FRAMES
-      envsubst < ./job_navhard_eval_dd.yaml | kubectl apply -f -
+      envsubst < ./job_navhard_eval_dd_simscale.yaml | kubectl apply -f -
     done
   done
 done
